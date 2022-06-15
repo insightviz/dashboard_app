767219b7a69bfdae7325619ec86adf2396dd6fb0
Eze Ahunanya <ezeahunanya@outlook.com>
2022-06-15 09:18:57 +0100

    Add logging for requests and handle bad responses

--- api_client/stop_search_data.py
+++ api_client/stop_search_data.py
@@ -1,7 +1,7 @@
 import httpx
 import asyncio
 from helper_functions import clean_data, HEADERS
-
+import time
 
 # get force data
 def get_forces():
@@ -36,37 +36,56 @@ def get_available_datasets():
                 available_data.append({'force_id': f.get('id'), 'month': i.get('date'), 
                 'force_name': f.get('name')})
     
-    return available_data
+    return available_data[:200] # remove indexing
 
 
 async def request_available_datasets():
-    available_datasets = get_available_datasets()[:10]
+    available_datasets = get_available_datasets()
 
-    async with httpx.AsyncClient() as client:
+    async with httpx.AsyncClient(timeout=None, event_hooks={'request': [log_request], 'response': [log_response]}) as client:
         tasks = []
         for i in available_datasets:
             parameters = {'force': i['force_id'], 'date': i['month']}
             force = i['force_name']
             task = asyncio.create_task(get_requests(client, parameters, force))
             tasks.append(task)
+            #pause before next request
+            await asyncio.sleep(.3)
 
-        data = await asyncio.gather(*tasks)
-        return data
+        response = await asyncio.gather(*tasks)
+    return response
 
+async def log_request(request):
+    print(f"Request event hook: {request.method} {request.url} - Waiting for response")
+    
 
+async def log_response(response):
+    request = response.request
+    print(f"Response event hook: {request.method} {request.url} - Status {response.status_code}")
 
 async def get_requests(client:httpx.AsyncClient, parameters:dict, force):
     '''This function sends requests to police-api for stop and search data. 
     Input is a list of tuples with arguments, month and police force.''' 
     
     url = 'https://data.police.uk/api/stops-force'
-    r = await client.get(url, headers=HEADERS, params=parameters)
+    response = await client.get(url, headers=HEADERS, params=parameters)
+    if response.status_code == 429 or response.status_code == 500:
+        await asyncio.sleep(2)
+        await get_requests(client, parameters, force)
 
-    result = await clean_data(r.json(), force)
-    return result
+    elif response.status_code == 200:
+        result = await clean_data(response.json(), force)
 
+    else:
+        response.raise_for_status()
+        
+        return result
+
+s = time.time()
 data = asyncio.run(request_available_datasets())
-print(data)
+e = time.time()
+#print(data)
 print(len(data))
+print(e-s)
 
 # save to database
