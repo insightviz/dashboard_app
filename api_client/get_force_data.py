d9caa31c016dbae8bebc6b1c90f2d12170ea5e7a
Eze Ahunanya <ezeahunanya@outlook.com>
2022-06-17 11:25:26 +0100

    Add script to save police forces into database

---
+++ api_client/get_force_data.py
@@ -0,0 +1,33 @@
+import httpx
+from helper_functions import HEADERS
+import time
+from sqlalchemy.orm import sessionmaker
+from database import PoliceForces, create_tables, engine
+
+
+# get force data
+def get_forces():
+    url = 'https://data.police.uk/api/forces'
+    r = httpx.get(url, headers=HEADERS)
+    return r.json()
+
+
+# save to database
+def save_forces_data_db():
+    response = get_forces()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for dictionary in response:
+        record = PoliceForces(*tuple(dictionary.values()))
+        print(f"Adding {dictionary['id']} police force to database...")
+        session.add(record)
+    print('Adding all records...')
+    session.commit()
+
+    
+if __name__ == '__main__':
+    create_tables()
+    s = time.time()
+    save_forces_data_db()
+    e = time.time()
+    print(e-s)
