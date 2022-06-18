d7b7331bdf2de42d05abef25211c585e34d367c9
Brook <71849503+Primebrook@users.noreply.github.com>
2022-06-18 20:06:06 +0100

    no need for print statements since sqlalchemy logs

--- scripts/get_available_datasets.py
+++ scripts/get_available_datasets.py
@@ -1,7 +1,6 @@
 import httpx
-import sqlalchemy
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
-from db.schema import AvailableData, engine
 from typing import List
 
 import os
@@ -9,6 +8,7 @@ import sys
 ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
 sys.path = [ROOT_DIR, ROOT_DIR+'/police_dashboard'] + sys.path
 
+from db.schema import AvailableData, engine
 from utils.helper_functions import load_from_json
 
 
@@ -22,16 +22,14 @@ def get_availabilty() -> List[dict]:
 def get_available_datasets() -> List[dict]:
     '''This function returns a list of tuples with police force and month where data 
     is available.'''
-
     available_data = []
-    police_forces = load_from_json(ROOT_DIR+'police_dashboard/data/police_forces.json')
+    police_forces = load_from_json(ROOT_DIR+'/police_dashboard/data/police_forces.json')
     available_dates = get_availabilty()
 
     for i in available_dates:
         for force_id in police_forces.keys():
             if force_id in i.get('stop-and-search'):
                 available_data.append({'force_id': force_id, 'month': i.get('date')})
-                
     return available_data
 
 def save_available_data_db() -> None:
@@ -39,14 +37,14 @@ def save_available_data_db() -> None:
     with Session(engine) as session:
         for available_data_record in response:
             record = AvailableData(**available_data_record)
-            print(f"Adding {available_data_record['force_id']} police force for {available_data_record['month']} to database...")
             session.add(record)
-        print('Adding all records...')
         session.commit()
     
     
+
 if __name__ == '__main__':
     try:
         save_available_data_db()
-    except sqlalchemy.exc.IntegrityError:
-        print('Error')
+    except IntegrityError as e:
+        print('\nINTEGRITY ERROR:')
+        print(e)
