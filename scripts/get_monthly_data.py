6051bf70b4a4511f2ee7f1f1e10575024095c5d0
Eze Ahunanya <ezeahunanya@outlook.com>
2022-06-21 11:21:56 +0100

    Move get availability function to new file

--- scripts/get_monthly_data.py
+++ scripts/get_monthly_data.py
@@ -1,3 +1,4 @@
+import httpx
 import time
 import asyncio
 from sqlalchemy.orm import Session
@@ -9,8 +10,30 @@ ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
 sys.path = [ROOT_DIR, ROOT_DIR+'/police_dashboard'] + sys.path
 
 from db.schema import StopSearchRecords, engine
-from scripts.get_available_datasets import get_available_datasets
 from scripts.get_stop_search_data import save_stop_search_data_db, request_available_datasets
+from utils.helper_functions import load_from_json
+
+
+def get_availabilty() -> list[dict]:
+    '''This function returns a list of dictionaries with the month as a key and 
+    list of police forces that proided stop and search data as the value.'''
+    url = 'https://data.police.uk/api/crimes-street-dates'
+    r = httpx.get(url)
+    return r.json()
+
+
+def get_available_datasets() -> list[dict]:
+    '''This function returns a list of dictionaries with police force and month where data 
+    is available.'''
+    available_data = []
+    police_forces = load_from_json(ROOT_DIR+'/police_dashboard/data/police_forces.json')
+    available_dates = get_availabilty()
+
+    for i in available_dates:
+        for force_id in police_forces.keys():
+            if force_id in i.get('stop-and-search'):
+                available_data.append({'force': force_id, 'month': i.get('date')})
+    return available_data
 
 
 def check_datasets_saved() -> set:
