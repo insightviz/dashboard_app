145eb5e54374543d93f361ab010c5c3673f22ad4
Eze Ahunanya <ezeahunanya@outlook.com>
2022-06-25 23:07:23 +0100

    Add html templates

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 
 def create_app(test_config=None):
@@ -27,6 +27,11 @@ def create_app(test_config=None):
     # a simple page that says hello
     @app.route('/')
     def index():
-        return 'Hello, World!'
+        return render_template('index.html')
+
+
+    @app.route('/contact')
+    def contact():
+        return render_template('contact.html')
 
     return app
