fce16701b069abfa09fad7a18c53baa4af8a866c
Eze Ahunanya <ezeahunanya@outlook.com>
2022-07-03 20:31:29 +0100

    Refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './Pages/Layout';
-import Home from './Pages/Home';
-import Dashboards from './Pages/Dashboards';
-import Contact from './Pages/Contact';
+import Layout from './Shared/Layout';
+import Home from './Home/Home';
+import Contact from './Contact/Contact';
+import StopSearch from './StopSearch/StopSearch';
 
 export default function App() {
   return (
@@ -12,7 +12,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
-          <Route path="dashboards" element={<Dashboards />} />
+          <Route path="/stopsearch" element={<StopSearch />} />
           <Route path="contact" element={<Contact />} />
         </Route>
       </Routes>
