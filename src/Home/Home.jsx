7c23e825bc69f36ee8ff4ccb9002a74b1f636abf
Eze Ahunanya <ezeahunanya@outlook.com>
2022-07-03 21:14:44 +0100

    Add link to dashboard to home

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 function Home() {
     return (
-        <h1>This is the homepage</h1>
+        <div>
+        <div>
+            <h1>This is the homepage</h1>
+        </div>
+        <div>
+            <ul>
+                <li><Link to="/stopsearch">Stop and search dashboard</Link></li>
+            </ul>
+        </div>
+        </div>
     );
 }
 
