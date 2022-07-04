c0cdac20e276a0f33f411052b8ab821be2640d40
Eze Ahunanya <ezeahunanya@outlook.com>
2022-07-04 23:47:03 +0100

    Fetch data from backend to plot

--- src/StopSearch/Controller/Controller.jsx
+++ src/StopSearch/Controller/Controller.jsx
@@ -4,18 +4,29 @@ import View1 from '../View1/View1';
 
 const DashboardController = () => {
   const [force, setForce] = useState('all');
+  const [data, setData] = useState(null);
+  const [isLoading, setLoading] = useState(true);
+   
 
   useEffect(() => {
     fetch('http://localhost:5000/stopsearch')
     .then(response => response.json())
-    .then(data => console.log(data));
-
-  }, [])
+    .then(data => {
+      console.log(data)
+      setData(data)
+      setLoading(false)
+    });
+  },[force])
+  
 
   return (
-    <View1 />
+    <div>
+      {isLoading || !data ? (
+        <div>loading...</div>
+      ) : (<View1 data={data}/>)}
+      
+    </div>
   )
-
 }
 
 export default DashboardController
