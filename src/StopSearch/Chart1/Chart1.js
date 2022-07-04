363db7ec5f474cbbd31c7a79875a48f5253c2242
Eze Ahunanya <ezeahunanya@outlook.com>
2022-07-04 15:30:30 +0100

    Change chart component from class to function

--- src/StopSearch/Chart1/Chart1.js
+++ src/StopSearch/Chart1/Chart1.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import Plot from 'react-plotly.js';
 
-class Chart1 extends React.Component {
-  render() {
-    return (
-      <Plot
-        data={[
-          {
-            x: [1, 2, 3],
-            y: [2, 6, 3],
-            type: 'scatter',
-            mode: 'lines+markers',
-            marker: {color: 'red'},
-          },
-          {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
-        ]}
-        layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
-      />
-    );
-  }
+function Chart1() {
+  return (
+    <Plot
+      data={[
+        {
+          x: [1, 2, 3],
+          y: [2, 6, 3],
+          type: 'scatter',
+          mode: 'lines+markers',
+          marker: {color: 'red'},
+        },
+        {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+      ]}
+      layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
+    />
+  );
 }
 
 export default Chart1;
