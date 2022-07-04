c1ef17de23e68d1262812f839b7e7f9c24940e63
Eze Ahunanya <ezeahunanya@outlook.com>
2022-07-04 23:28:24 +0100

    Add props to chart component

--- src/StopSearch/Chart1/Chart1.jsx
+++ src/StopSearch/Chart1/Chart1.jsx
@@ -1,17 +1,10 @@
 import Plot from 'react-plotly.js';
 
-function Chart1() {
+function Chart1(props) {
   return (
     <Plot
       data={[
-        {
-          x: [1, 2, 3],
-          y: [2, 6, 3],
-          type: 'scatter',
-          mode: 'lines+markers',
-          marker: {color: 'red'},
-        },
-        {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+        props.data,
       ]}
       layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
     />
