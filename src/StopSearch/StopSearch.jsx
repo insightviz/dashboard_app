ef391617f101e2f5a7007ce138fc0da947b29b60
Eze Ahunanya <ezeahunanya@outlook.com>
2022-07-04 15:26:54 +0100

    Add first view in dashboard app

--- src/StopSearch/StopSearch.jsx
+++ src/StopSearch/StopSearch.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import App from './App';
+import View1 from './View1/View1';
 
 function StopSearch() {
     return (
         <div>
           <h1>This is the stop and search dashboard</h1>
-          <App />
-        </div>
-        
+          <View1 />
+        </div> 
     );
 }
 
---
+++ src/StopSearch/View1/View1.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Chart1 from '../Chart1/Chart1';
+
+function View1() {
+  return (
+    <Chart1/>
+  );
+}
+
+export default View1;
