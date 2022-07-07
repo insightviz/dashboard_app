211d7807da4e5d955c2c50e6e9031334224e3a5c
Eze Ahunanya <ezeahunanya@outlook.com>
2022-07-07 22:30:54 +0100

    Initial commit, add sign up form to website

--- src/Shared/Layout.jsx
+++ src/Shared/Layout.jsx
@@ -1,11 +1,13 @@
 import {Outlet} from "react-router-dom";
 import Navbar from "./Navbar";
+import SignUpForm from "./SignUpForm/SignUpForm";
 
 const Layout = () => {
   return (
     <>
       <Navbar />
       <Outlet />
+      <SignUpForm />
     </>
   );
 };
---
+++ src/Shared/SignUpForm/SignUpForm.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+function SignUpForm () {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [email, setEmail] = useState('');
+        
+    const handleChange = (e) => {
+      setFirstName(e.target.firstName);
+      setLastName(e.target.lastName);
+      setEmail(e.target.email);
+    }
+  
+    const handleSubmit = (e) => {
+      alert('A name was submitted: ' + firstName);
+      e.preventDefault();
+    }
+  
+    
+    return (
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+            <legend>Sign up to keep updated about new dashboards!</legend>
+            <label htmlFor="firstName">
+              First Name:
+              <input type="text" value={firstName} onChange={handleChange} id="firstName" name="first_name" placeholder="First Name" required />
+            </label>
+            <label htmlFor="lastName">
+              Last Name:
+              <input type="text" value={lastName} onChange={handleChange} id="lastName" name="last_name" placeholder="Last Name" required/>
+            </label>
+            <label htmlFor="email">
+              Email:
+              <input type="email" value={email} onChange={handleChange} id="email" name="email" placeholder="you@example.com"/>
+            </label>
+            <input type="submit" value="Sign Up" />
+        </fieldset>
+      </form>
+    );
+}
+
+export default SignUpForm;
