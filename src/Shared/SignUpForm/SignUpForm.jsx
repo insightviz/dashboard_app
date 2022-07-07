4b1d4e779956710adb14ab73ae1276518b5d08c9
Eze Ahunanya <ezeahunanya@outlook.com>
2022-07-07 23:35:18 +0100

    Add submitting pop up on submit

--- src/Shared/SignUpForm/SignUpForm.jsx
+++ src/Shared/SignUpForm/SignUpForm.jsx
@@ -4,6 +4,7 @@ function SignUpForm () {
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [email, setEmail] = useState('');
+    const [isSubmitting, setSubmitting] = useState(false);
         
     const handleFirstNameChange = (e) => {
       setFirstName(e.target.value);
@@ -18,30 +19,40 @@ function SignUpForm () {
     }
   
     const handleSubmit = (e) => {
-      alert('A name was submitted: ' + firstName);
       e.preventDefault();
+      setSubmitting(true);
+
+      setTimeout(() => {
+        setSubmitting(false);
+      }, 1500)
+ 
     }
   
     
     return (
-      <form onSubmit={handleSubmit}>
-        <fieldset>
-            <legend>Sign up to keep updated about new dashboards!</legend>
-            <label htmlFor="firstName">
-              First Name:
-              <input type="text" value={firstName} onChange={handleFirstNameChange} id="firstName" name="first_name" placeholder="First Name" required />
-            </label>
-            <label htmlFor="lastName">
-              Last Name:
-              <input type="text" value={lastName} onChange={handleLastNameChange} id="lastName" name="last_name" placeholder="Last Name" required/>
-            </label>
-            <label htmlFor="email">
-              Email:
-              <input type="email" value={email} onChange={handleEmailChange} id="email" name="email" placeholder="you@example.com" required/>
-            </label>
-            <input type="submit" value="Sign Up" />
-        </fieldset>
-      </form>
+      <div>
+          {isSubmitting &&
+              <div>Submtting Form...</div>
+            }
+          <form onSubmit={handleSubmit}>
+            <fieldset>
+                <legend>Sign up to keep updated about new dashboards!</legend>
+                <label htmlFor="firstName">
+                  First Name:
+                  <input type="text" value={firstName} onChange={handleFirstNameChange} id="firstName" name="first_name" placeholder="First Name" required />
+                </label>
+                <label htmlFor="lastName">
+                  Last Name:
+                  <input type="text" value={lastName} onChange={handleLastNameChange} id="lastName" name="last_name" placeholder="Last Name" required/>
+                </label>
+                <label htmlFor="email">
+                  Email:
+                  <input type="email" value={email} onChange={handleEmailChange} id="email" name="email" placeholder="you@example.com" required/>
+                </label>
+                <input type="submit" value="Sign Up" />
+            </fieldset>
+          </form>
+      </div>
     );
 }
 
