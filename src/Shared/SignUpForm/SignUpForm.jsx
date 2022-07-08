ae6b99a32378712629668e29d17c2826442915ea
Eze Ahunanya <ezeahunanya@outlook.com>
2022-07-08 06:18:15 +0100

    Add pattern validation to first and last name

--- src/Shared/SignUpForm/SignUpForm.jsx
+++ src/Shared/SignUpForm/SignUpForm.jsx
@@ -15,20 +15,20 @@ function SignUpForm ({onSubmit}) {
             <div className="name-inputs">
               <div className="first-name-input">
                 <label htmlFor="firstName">First Name</label>
-                <input {...register("firstName", { required: 'First name is required'})} type="text" placeholder="First Name" />
-                {errors.firstName && <p>First name is required.</p>}
+                <input {...register("firstName", { required: 'First name is required', pattern: {value: /[A-Za-z]+/, message: 'Invalid first name'}})} type="text" placeholder="First Name" />
+                <p>{errors.firstName?.message}</p>
               </div>
               <div className="last-name-input">
                 <label htmlFor="lastName">Last Name</label>
-                <input {...register("lastName", { required: 'Last name is required'})} type="text" placeholder="Last Name" />
-                {errors.lastName && <p>Last name is required.</p>}
+                <input {...register("lastName", { required: 'Last name is required', pattern: {value: /[A-Za-z]+/, message: 'Invalid last name'}})} type="text" placeholder="Last Name" />
+                <p>{errors.lastName?.message}</p>
               </div>
             </div>
                 
             <div className="email-input">
               <label htmlFor="email">Email</label>
               <input {...register("email", { required: 'Email is required'})} type="email" placeholder="you@example.com" />
-              {errors.email && <p>Email is required.</p>}
+              <p>{errors.lastName?.message}</p>
             </div>
             
             <div className="submit-button">
