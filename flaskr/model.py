9906e782a20698866d1a873b3cac2f08b0bedf7a
Eze Ahunanya <ezeahunanya@outlook.com>
2022-07-08 23:36:14 +0100

    Change column names in db model

--- flaskr/model.py
+++ flaskr/model.py
@@ -5,6 +5,6 @@ db = SQLAlchemy()
 class User(db.Model):
     __tablename__ = 'users_registered'
     id = db.Column(db.Integer, primary_key =True)
-    first_name = db.Column(db.String, nullable = False)
-    last_name = db.Column(db.String, nullable = True)
+    firstName = db.Column(db.String, nullable = False)
+    lastName = db.Column(db.String, nullable = True)
     email = db.Column(db.String, nullable = False)
