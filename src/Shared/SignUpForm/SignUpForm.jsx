7d9142fb0d2d898519c41b3c6476e423ef7eacb1
Eze Ahunanya <ezeahunanya@outlook.com>
2022-07-08 05:51:17 +0100

    Change error tags

--- src/Shared/SignUpForm/SignUpForm.jsx
+++ src/Shared/SignUpForm/SignUpForm.jsx
@@ -16,19 +16,19 @@ function SignUpForm ({onSubmit}) {
               <div className="first-name-input">
                 <label htmlFor="firstName">First Name</label>
                 <input {...register("firstName", { required: 'First name is required'})} type="text" placeholder="First Name" />
-                <p>{errors.firstName?.message}</p>
+                {errors.firstName && <p>First name is required.</p>}
               </div>
               <div className="last-name-input">
                 <label htmlFor="lastName">Last Name</label>
                 <input {...register("lastName", { required: 'Last name is required'})} type="text" placeholder="Last Name" />
-                <p>{errors.lastName?.message}</p>
+                {errors.lastName && <p>Last name is required.</p>}
               </div>
             </div>
                 
             <div className="email-input">
               <label htmlFor="email">Email</label>
               <input {...register("email", { required: 'Email is required'})} type="email" placeholder="you@example.com" />
-              <p>{errors.email?.message}</p>
+              {errors.email && <p>Email is required.</p>}
             </div>
             
             <div className="submit-button">
