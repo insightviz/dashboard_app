9b0e1a2e3dc7f09977aec076fc8c35b3dc03c8aa
Eze Ahunanya <ezeahunanya@outlook.com>
2022-07-08 04:56:57 +0100

    Separate app from index file

--- src/index.jsx
+++ src/index.jsx
@@ -1,24 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from './Shared/Layout';
-import Home from './Home/Home';
-import Contact from './Contact/Contact';
-import StopSearch from './StopSearch/StopSearch';
-
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="/stopsearch" element={<StopSearch />} />
-          <Route path="/contact" element={<Contact />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
