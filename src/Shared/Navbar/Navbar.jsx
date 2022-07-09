b211a0cd5f8e6dd773a68c294c3fbadcbe27a3b9
Eze Ahunanya <ezeahunanya@outlook.com>
2022-07-09 07:01:42 +0100

    Add logo to nav bar

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,8 +1,14 @@
 import { Link } from "react-router-dom";
+import logo from "./insight_logo.svg"
 
 function Navbar() {
   return (
     <nav>
+      <div className="logo-container">
+        <Link to="/">
+          <img id="insight" src={logo}/>
+        </Link>
+      </div>
       <ul>
         <li>
           <Link to="/">Home</Link>
