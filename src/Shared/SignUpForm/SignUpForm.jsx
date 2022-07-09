2753e7f6ed52fa94e27b48b09c3b29e25d56ffbe
Eze Ahunanya <ezeahunanya@outlook.com>
2022-07-09 04:20:56 +0100

    Initial commit, add styling to sign up form

--- src/Shared/SignUpForm/SignUpForm.jsx
+++ src/Shared/SignUpForm/SignUpForm.jsx
@@ -1,4 +1,5 @@
 import { useForm } from "react-hook-form";
+import './SignUpForm.css'
 
 function SignUpForm ({onSubmit}) {
     const { register, handleSubmit, formState: { errors } } = useForm();
@@ -7,24 +8,28 @@ function SignUpForm ({onSubmit}) {
     return (
       <div>
           <form onSubmit={handleSubmit(onSubmit)}>
-            <legend>Sign up to keep updated about new dashboards!</legend>
+            <div className="form-title">
+                <h3>Sign up to keep updated about new dashboards!</h3>
+            </div>
             <div className="name-inputs">
               <div className="first-name-input">
-                <label htmlFor="firstName">First Name</label>
+                <label htmlFor="firstName">Your first name</label>
                 <input {...register("firstName", { required: 'First name is required', pattern: {value: /[A-Za-z]+/, message: 'Invalid first name'}})} type="text" placeholder="First Name" />
                 <p>{errors.firstName?.message}</p>
               </div>
               <div className="last-name-input">
-                <label htmlFor="lastName">Last Name</label>
+                <label htmlFor="lastName">Your last name</label>
                 <input {...register("lastName", { required: 'Last name is required', pattern: {value: /[A-Za-z]+/, message: 'Invalid last name'}})} type="text" placeholder="Last Name" />
                 <p>{errors.lastName?.message}</p>
               </div>
             </div>
                 
             <div className="email-input">
-              <label htmlFor="email">Email</label>
-              <input {...register("email", { required: 'Email is required'})} type="email" placeholder="you@example.com" />
-              <p>{errors.lastName?.message}</p>
+              <div>
+                <label htmlFor="email">Your email address</label>
+                <input {...register("email", { required: 'Email is required'})} type="email" placeholder="you@example.com" />
+                <p>{errors.lastName?.message}</p>
+              </div>
             </div>
             
             <div className="submit-button">
