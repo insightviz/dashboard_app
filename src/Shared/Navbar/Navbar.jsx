18c15ca7207e0cf67a3afdcd5330b67c4ac3e19b
Eze Ahunanya <ezeahunanya@outlook.com>
2022-07-09 07:02:41 +0100

    Delete home from nav bar

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -11,9 +11,6 @@ function Navbar() {
       </div>
       <ul>
         <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
           <Link to="/contact">Contact</Link>
         </li>
       </ul>
