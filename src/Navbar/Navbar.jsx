180146f9096bbcdf710d670bb22f70fa915e668d
Eze Ahunanya <ezeahunanya@outlook.com>
2022-07-12 14:37:27 +0100

    Add styling to navbar

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,25 +1,37 @@
 import { Link } from "react-router-dom";
 import logo from "./insight_logo_white.svg"
 import "./Navbar.css"
+import { FaBars, FaTimes } from "react-icons/fa";
+import { useState } from 'react';
 
 function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click)
+
+  const closeMobileMenu = () => setClick(false)
   return (
     <nav className="navbar">
-      <div className="logo-container">
-        <Link to="/">
-          <img id="insight" src={logo}/>
-        </Link>
-      </div>
-      <div className="navbar-links">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-        </ul>
-      </div>
+      <div className="navbar-container">
+        <div >
+          <Link to="/" className="logo-container">
+            <img id="insight" src={logo}/>
+          </Link>
+        </div>
+        <div className="menu-icon" onClick={handleClick}>
+          {click ? <FaTimes/> : <FaBars/>}
+        </div>
+        <div>
+          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/contact" className="nav-links" onClick={closeMobileMenu}>Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </div> 
     </nav>
   );
 }
