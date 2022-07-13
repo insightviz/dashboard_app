f9bb8603d1f9352440c48c793af00fd3ebaa5485
Eze Ahunanya <ezeahunanya@outlook.com>
2022-07-13 20:14:31 +0100

    Add controller for signup form to separate logic

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,27 +1,15 @@
 import {Outlet} from "react-router-dom";
 import NavbarController from "../Navbar/NavbarController";
-import SignUpForm from "../SignUpForm/SignUpForm";
+import SignUpController from "../SignUpForm/SignUpController";
+
 import "./Layout.css"
 
 const Layout = () => {
-
-  const onSubmit = (data) => {
-    fetch("http://localhost:5000/signup", {
-     
-    method: "POST",
-    body: JSON.stringify(data),
-    headers: {
-        "Content-Type": "application/json"
-      }
-    }).then(response => response.text())
-    .then(string => alert(string))
-  }
-
   return (
     <>
-      <NavbarController />
+      <NavbarController/>
       <Outlet />
-      <SignUpForm onSubmit={onSubmit}/>
+      <SignUpController/>
     </>
   );
 };
