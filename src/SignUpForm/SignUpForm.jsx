1117fac432ceff3f15029dbeab35ff05b197f775
Eze Ahunanya <ezeahunanya@outlook.com>
2022-07-15 15:47:40 +0100

    Change styling of signup form

--- src/SignUpForm/SignUpForm.jsx
+++ src/SignUpForm/SignUpForm.jsx
@@ -11,7 +11,7 @@ function SignUpForm ({onSubmit}) {
             <div className="form-title">
                 <h3>Subscribe for updates on new dashboards!</h3>
             </div>
-            <div className="name-inputs">
+            <div className="inputs">
               <div className="first-name-input">
                 <label htmlFor="firstName">Your first name</label>
                 <input {...register("firstName", { required: 'First name is required', pattern: {value: /[A-Za-z]+/, message: 'Invalid first name'}})} type="text" placeholder="First Name" />
@@ -22,16 +22,15 @@ function SignUpForm ({onSubmit}) {
                 <input {...register("lastName", { required: 'Last name is required', pattern: {value: /[A-Za-z]+/, message: 'Invalid last name'}})} type="text" placeholder="Last Name" />
                 <p>{errors.lastName?.message}</p>
               </div>
-            </div>
-                
-            <div className="email-input">
-              <div>
-                <label htmlFor="email">Your email address</label>
-                <input {...register("email", { required: 'Email is required'})} type="email" placeholder="you@example.com" />
-                <p>{errors.lastName?.message}</p>
+              <div className="email-input">
+                <div>
+                  <label htmlFor="email">Your email address</label>
+                  <input {...register("email", { required: 'Email is required'})} type="email" placeholder="you@example.com" />
+                  <p>{errors.email?.message}</p>
+                </div>
               </div>
             </div>
-            
+                           
             <div className="submit-button">
               <input type="submit" value="Subscribe" />
             </div>
