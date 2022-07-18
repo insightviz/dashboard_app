2b556dce35456d2e781a49ef7b3d216d46772d80
Eze Ahunanya <ezeahunanya@outlook.com>
2022-07-18 14:13:06 +0100

    Fix layout.jsx import

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -1,6 +1,6 @@
 import {Outlet} from "react-router-dom";
 import NavbarController from "./Navbar/NavbarController";
-import SignUpController from "./SignUpForm/SignUpController";
+import SignUpController from "../Components/SignUpForm/SignUpController";
 
 import "./Layout.css"
 
