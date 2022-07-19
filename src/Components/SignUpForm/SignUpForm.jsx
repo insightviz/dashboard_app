751db04bd1340b416bc0b20c6dfb03dd226f38d3
Eze Ahunanya <ezeahunanya@outlook.com>
2022-07-19 05:50:00 +0100

    Add icons inputs and styling

--- src/Components/SignUpForm/SignUpForm.jsx
+++ src/Components/SignUpForm/SignUpForm.jsx
@@ -1,5 +1,6 @@
 import { useForm } from "react-hook-form";
 import './SignUpForm.css'
+import { MdAlternateEmail, MdPersonOutline } from "react-icons/md";
 
 function SignUpForm ({onSubmit}) {
     const { register, handleSubmit } = useForm();
@@ -10,17 +11,21 @@ function SignUpForm ({onSubmit}) {
             <h3>Subscribe for updates on new dashboards!</h3>
           </div>
           <div className="inputs">
-            <div className="first-name-input">
-              <input {...register("firstName")} type="text" placeholder="First Name" />
-            </div>
-            <div className="email-input">
-              <div>
-                <input {...register("email", { required: 'Email is required'})} type="email" placeholder="Email Address" />
+            <div className="first-name-container">
+              <div className="first-name-icon">
+                <MdPersonOutline size="2rem"/>
               </div>
+              <input className="first-name-input" {...register("firstName")} type="text" placeholder="First Name" />
             </div>
-          </div>           
-          <div className="submit-button">
-            <input type="submit" value="Subscribe" />
+            <div className="email-container">
+              <div className="email-icon">
+                <MdAlternateEmail size="2rem"/>
+              </div>
+              <input className="email-input" {...register("email", { required: 'Email is required'})} type="email" placeholder="Email Address" />
+            </div>
+            <div className="submit-button">
+              <input type="submit" value="Subscribe" />
+            </div>
           </div>
         </form>
       </section>
