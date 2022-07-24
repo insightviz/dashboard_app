5a482960d0bd07e4722bd82928c5693a0317c8ef
Eze Ahunanya <ezeahunanya@outlook.com>
2022-07-24 23:07:11 +0100

    Refactor global css file change

--- src/Layout/Layout.jsx
+++ src/Layout/Layout.jsx
@@ -2,8 +2,6 @@ import {Outlet} from "react-router-dom";
 import NavbarController from "./Navbar/NavbarController";
 import Footer from "./Footer/Footer";
 
-import "./Layout.css"
-
 const Layout = () => {
   return (
     <>
--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
 import ThemeProvider from './AppTheme';
+import "./index.css"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
