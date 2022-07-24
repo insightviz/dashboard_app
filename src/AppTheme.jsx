cf70bbbf95539792241d7ff69633a9d5af077ada
Eze Ahunanya <ezeahunanya@outlook.com>
2022-07-24 12:32:32 +0100

    Add system, dark and light modes

--- src/AppTheme.jsx
+++ src/AppTheme.jsx
@@ -3,28 +3,44 @@ import App from './App'
 
 function AppTheme() {
   // This holds the information about dark mode/light mode
-  const getCurrentTheme = () => window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
-  const [mode, setMode] = useState(getCurrentTheme());
-
-  const handleThemeChange = (e) => {
-    if (e.value==='system') {
-      setMode(getCurrentTheme())
-    } else {
-      setMode(e.value)
+  const initialMode = () => { return "system" }
+  const [mode, setMode] = useState(initialMode);
+  const [theme, setTheme] = useState(() => {
+    if (mode !== "system") {
+      return mode;
     }
+    const isSystemInDarkMode = matchMedia("(prefers-color-scheme: dark)")
+      .matches;
+    return isSystemInDarkMode ? "dark" : "light";
+  });
+    
+  const handleModeChange = (e) => {
+    setMode(e.value)    
   }
 
   useEffect(() => {
-    window.matchMedia('(prefers-color-scheme: dark)')
-    .addEventListener('change', event => {
-        const colorScheme = event.matches ? "dark" : "light";
-        setMode(colorScheme);// "dark" or "light"
-
-      });
-  }, []);
+    if (mode !== "system") {
+      setTheme(mode);
+      return;
+    }
+  
+    const isSystemInDarkMode = matchMedia("(prefers-color-scheme: dark)");
+    // If system mode, immediately change theme according to the current system value
+    setTheme(isSystemInDarkMode.matches ? "dark" : "light");
+  
+    // As the system value can change, we define an event listener when in system mode
+    // to track down its changes
+    const listener = (event: MediaQueryListEvent) => {
+      setTheme(event.matches ? "dark" : "light");
+    };
+    isSystemInDarkMode.addEventListener('change', listener);
+    return () => {
+      isSystemInDarkMode.removeEventListener('change', listener);
+    };
+  }, [mode]);
 
   return (
-    <App mode={mode} onChange={handleThemeChange}/>
+    <App mode={mode} onChange={handleModeChange}/>
   );
 }
 
