7a003b8f468a4532b7e340ba6cb03d013651b0fe
Eze Ahunanya <ezeahunanya@outlook.com>
2022-07-27 22:56:20 +0100

    Add lazy loading to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './Layout/Layout';
-import Home from './Pages/Home/Home';
-import Contact from './Pages/Contact/Contact';
-import StopSearch from './Pages/StopSearch/StopSearch';
+import { lazy, Suspense } from 'react';
+
+const Home = lazy(() => import('./Pages/Home/Home'));
+const Contact = lazy(() => import('./Pages/Contact/Contact'));
+const StopSearch = lazy(() => import('./Pages/StopSearch/StopSearch'));
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout/>}>
-          <Route index element={<Home />} />
-          <Route path="/stopsearch" element={<StopSearch />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route index element={
+            <Suspense fallback={<>Loading...</>}>
+              <Home />
+            </Suspense>} />
+          <Route path="/stopsearch" element={
+            <Suspense fallback={<>Loading...</>}>
+              <StopSearch />
+            </Suspense>} />
+          <Route path="/contact" element={
+            <Suspense fallback={<>Loading...</>}>
+              <Contact />
+            </Suspense>} />
         </Route>
       </Routes>
     </BrowserRouter>
