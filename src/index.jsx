a1c9b97552c81e02a50e9874631c3a6a3d3d8484
Eze Ahunanya <ezeahunanya@outlook.com>
2022-07-28 06:28:41 +0100

    Add @geist-ui/core provider to app

--- src/index.jsx
+++ src/index.jsx
@@ -4,13 +4,17 @@ import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
 import ThemeProvider from './AppTheme';
 import "./index.css"
+import { GeistProvider, CssBaseline } from '@geist-ui/core'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ChakraProvider>
       <ThemeProvider>
-        <App />
+        <GeistProvider>
+          <CssBaseline />
+          <App />
+        </GeistProvider>
       </ThemeProvider>
     </ChakraProvider>
   </React.StrictMode>
