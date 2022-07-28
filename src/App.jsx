fd1fd28021fe6b561b4667a1b951dd43975cb7b5
Eze Ahunanya <ezeahunanya@outlook.com>
2022-07-28 08:42:39 +0100

    Add theme to geist provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,38 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './Layout/Layout';
-import { lazy, Suspense } from 'react';
+import { lazy, Suspense, useContext } from 'react';
+import { ThemeContext } from './AppTheme';
+import { GeistProvider, CssBaseline } from '@geist-ui/core'
+
 
 const Home = lazy(() => import('./Pages/Home/Home'));
 const Contact = lazy(() => import('./Pages/Contact/Contact'));
 const StopSearch = lazy(() => import('./Pages/StopSearch/StopSearch'));
 
 export default function App() {
+  const { theme } = useContext(ThemeContext);
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout/>}>
-          <Route index element={
-            <Suspense fallback={<>Loading...</>}>
-              <Home />
-            </Suspense>} />
-          <Route path="/stopsearch" element={
-            <Suspense fallback={<>Loading...</>}>
-              <StopSearch />
-            </Suspense>} />
-          <Route path="/contact" element={
-            <Suspense fallback={<>Loading...</>}>
-              <Contact />
-            </Suspense>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <GeistProvider themeType={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout/>}>
+            <Route index element={
+              <Suspense fallback={<>Loading...</>}>
+                <Home />
+              </Suspense>} />
+            <Route path="/stopsearch" element={
+              <Suspense fallback={<>Loading...</>}>
+                <StopSearch />
+              </Suspense>} />
+            <Route path="/contact" element={
+              <Suspense fallback={<>Loading...</>}>
+                <Contact />
+              </Suspense>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </GeistProvider>
   );
   }
--- src/index.jsx
+++ src/index.jsx
@@ -4,17 +4,13 @@ import App from './App';
 import { ChakraProvider } from '@chakra-ui/react';
 import ThemeProvider from './AppTheme';
 import "./index.css"
-import { GeistProvider, CssBaseline } from '@geist-ui/core'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ChakraProvider>
       <ThemeProvider>
-        <GeistProvider>
-          <CssBaseline />
-          <App />
-        </GeistProvider>
+        <App />
       </ThemeProvider>
     </ChakraProvider>
   </React.StrictMode>
