c5e3abafb2da59696f1e49b57f8cc09fa9494ef2
Eze Ahunanya <ezeahunanya@outlook.com>
2022-08-07 17:36:37 +0100

    Refactor cleaning code

--- dashboards/stop_search_dashboard/utils/helper_functions.py
+++ dashboards/stop_search_dashboard/utils/helper_functions.py
@@ -1,24 +1,5 @@
 import json
 
-REPLACE_DICT = {'Other ethnic group - Not stated': 'Other',
-         'Black/African/Caribbean/Black British - Any other Black/African/Caribbean background': 'Black',
-         'Black/African/Caribbean/Black British - African': 'Black',
-         'White - Any other White background': 'White',
-         'Other ethnic group - Any other ethnic group': 'Other',
-         'Other ethnic group - Arab': 'Other',
-         'Black/African/Caribbean/Black British - Caribbean': 'Black',
-         'Asian/Asian British - Pakistani': 'Asian',
-         'White - English/Welsh/Scottish/Northern Irish/British': 'White',
-         'Asian/Asian British - Any other Asian background': 'Asian',
-         'Asian/Asian British - Bangladeshi': 'Asian',
-         'Mixed/Multiple ethnic groups - Any other Mixed/Multiple ethnic background': 'Mixed',
-         'Mixed/Multiple ethnic groups - White and Asian': 'Mixed',
-         'Asian/Asian British - Indian': 'Asian',
-         'Mixed/Multiple ethnic groups - White and Black African': 'Mixed',
-         'Mixed/Multiple ethnic groups - White and Black Caribbean': 'Mixed',
-         'White - Irish': 'White', 
-         'White - Gypsy or Irish Traveller': 'White',
-         'Asian/Asian British - Chinese': 'Asian'}
 
 def get_dictionary_value(dictionary, keys):
     '''
@@ -68,12 +49,18 @@ async def clean_data(available_dataset, parameters:dict):
         item['street_desc'] = get_dictionary_value(item, ['location', 'street', 'name'])
         del item['location']
 
-        if item.get('self_defined_ethnicity') in REPLACE_DICT.keys():
-            item['person_ethnicity'] = REPLACE_DICT[item['self_defined_ethnicity']]
-        
+        if 'black' in str(item.get('self_defined_ethnicity')).lower():
+            item['person_ethnicity'] = 'Black'
+        elif 'white' in str(item.get('self_defined_ethnicity')).lower():
+            item['person_ethnicity'] = 'White'
+        elif 'asian' in str(item.get('self_defined_ethnicity')).lower():
+            item['person_ethnicity'] = 'Asian'
+        elif 'mixed' in str(item.get('self_defined_ethnicity')).lower():
+            item['person_ethnicity'] = 'Mixed'
+        elif 'other' in str(item.get('self_defined_ethnicity')).lower():
+            item['person_ethnicity'] = 'Other'
         else:
-            item['person_ethnicity'] = item['self_defined_ethnicity']
-
+            item['person_ethnicity'] = item['self_defined_ethnicity'] 
         list.append(item)
     return list
 
