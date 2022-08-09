e587bc45370ea4c4f7eed69027e9cb036e6f1a17
Eze Ahunanya <ezeahunanya@outlook.com>
2022-08-09 19:29:34 +0100

    Add monthly stop and searches to view

--- src/Pages/StopSearch/View1/View1.jsx
+++ src/Pages/StopSearch/View1/View1.jsx
@@ -3,6 +3,10 @@ import Chart from '../Chart/Chart';
 function View1({data}) {
   return (
     <div>
+      <div className="monthly-stop-searches">
+        {data.figure_1.monthly_no_stop_search}
+        {data.figure_1.pct_change}
+      </div>
       <Chart data={data.breakdown_by_race} title={'Monthly count of stop and searches'}/>
       <Chart data={data.breakdown_by_police_ethnicity} title={'Monthly count of stop and searches'}/>
       <Chart data={data.breakdown_of_object_of_search_by_ethnicity} title={'Monthly count of stop and searches'}/>
