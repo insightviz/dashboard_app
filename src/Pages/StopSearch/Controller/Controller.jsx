bc0700b42f07976fed31f3ec494178e6985837a7
Eze Ahunanya <ezeahunanya@outlook.com>
2022-08-10 00:15:29 +0100

    Add ethnicity dropdown

--- src/Pages/StopSearch/Controller/Controller.jsx
+++ src/Pages/StopSearch/Controller/Controller.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import View1 from '../View1/View1';
 import { Select } from "@geist-ui/core/";
+import Chart from '../Chart/Chart';
 
 const forceOptions = [
   {value: 'avon-and-somerset', label: 'Avon and Somerset Constabulary'},
@@ -49,18 +50,25 @@ const forceOptions = [
   {value: 'wiltshire', label: 'Wiltshire Police'}
 ]
 
+const ethnicityOptions = [
+  {value: 'Asian', label: 'Asian'},
+  {value: 'Black', label: 'Black'},
+  {value: 'Mixed', label: 'Mixed'},
+  {value: 'Other', label: 'Other'},
+  {value: 'White', label: 'White'}
+]
+
 const DashboardController = () => {
-  const initialForce = () => { return (localStorage.getItem("force")) || 'metropolitan' }
+  const initialForce = () => localStorage.getItem("force") || 'metropolitan' 
   const [force, setForce] = useState(initialForce);
-  const initialEthnicity = () => { return (localStorage.getItem("ethnicity")) || 'White' }
+  const initialEthnicity = () => localStorage.getItem("ethnicity") || 'White' 
   const [ethnicity, setEthnicity] = useState(initialEthnicity);
   
   const [data, setData] = useState(null);
-  const [isDataLoading, setDataLoading] = useState(true);
+  const [isView1DataLoading, setView1DataLoading] = useState(true);
+  const [isView2DataLoading, setView2DataLoading] = useState(true);
   
   const loadData = () => {
-    setDataLoading(true)
-
     let forceQueryString = force
     let ethnicityQueryString = ethnicity
     let url = `http://localhost:5000/stopsearch/data?force=${forceQueryString}&ethnicity=${ethnicityQueryString}`
@@ -69,14 +77,22 @@ const DashboardController = () => {
     .then(response => response.json())
     .then(data => {
       setData(data)
-      setDataLoading(false)
+      setView1DataLoading(false)
+      setView2DataLoading(false)
     });
   }
-  useEffect(loadData, [force])
+  useEffect(loadData, [force, ethnicity])
   
   const handleForceChange = (e) => {
+    setView1DataLoading(true)
     setForce(e);
     localStorage.setItem("force", e);
+  }
+
+  const handleEthnicityChange = (e) => {
+    setView2DataLoading(true)
+    setEthnicity(e);
+    localStorage.setItem("ethnicity", e);
   }
   
   return (
@@ -88,11 +104,41 @@ const DashboardController = () => {
           dropdownClassName='force-dropdown'
           aria-label={'select police force data'}
         >
-          {forceOptions.map((element) => 
-            <Select.Option key={element.value} value={element.value} aria-label={element.label}>{element.label}</Select.Option>
+          {forceOptions.map(({value, label}) => 
+            <Select.Option key={value} value={value} aria-label={label}>{label}</Select.Option>
           )}
       </Select>
-      {isDataLoading ? <div>loading...</div> : <View1 data={data}/>}           
+      {
+        isView1DataLoading ? 
+        <div>loading...</div> : 
+        <div className="view1">
+          <div className="monthly-stop-searches">
+            {data.figure_1.monthly_no_stop_search}
+            {data.figure_1.pct_change}
+          </div>
+          <Chart data={data.breakdown_by_race} title={'Monthly count of stop and searches'}/>
+        </div>
+      } 
+      <Select 
+          initialValue={ethnicity}
+          value={ethnicity}
+          onChange={handleEthnicityChange}
+          dropdownClassName='ethnicity-dropdown'
+          aria-label={'select police ethnicity data'}
+          >
+          {ethnicityOptions.map(({value, label}) => 
+            <Select.Option key={value} value={value} aria-label={label}>{label}</Select.Option>
+          )}
+      </Select>          
+      {
+        isView1DataLoading || isView2DataLoading ? 
+        <div>loading...</div> : 
+        <div className="view2">
+          <Chart data={data.breakdown_by_police_ethnicity} title={'Monthly count of stop and searches'}/>
+          <Chart data={data.breakdown_of_object_of_search_by_ethnicity} title={'Monthly count of stop and searches'}/>
+          <Chart data={data.breakdown_of_outcomes_by_ethnicity} title={'Monthly count of stop and searches'}/>
+        </div>
+      } 
     </div>
   )
 }
