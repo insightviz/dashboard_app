9dc50fd35ba3d4e1556cf5b4e5e426ad286476d9
Eze Ahunanya <ezeahunanya@outlook.com>
2022-08-23 03:37:43 +0100

    Change name of dashboard

--- src/Pages/StopSearch/StopSearch.jsx
+++ src/Pages/StopSearch/StopSearch.jsx
@@ -3,7 +3,7 @@ import DashboardController from './Controller/Controller'
 function StopSearch() {
     return (
         <div>
-          <h1>This is the stop and search dashboard</h1>
+          <h1>UK stop and search dashboard</h1>
           <DashboardController />
         </div> 
     );
