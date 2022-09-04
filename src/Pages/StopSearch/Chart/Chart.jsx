13a1fb0675dda208409e4e26795b0bc2ab34583a
Eze Ahunanya <ezeahunanya@outlook.com>
2022-09-04 05:52:34 +0100

    Add font colour on charts

--- src/Pages/StopSearch/Chart/Chart.jsx
+++ src/Pages/StopSearch/Chart/Chart.jsx
@@ -10,8 +10,8 @@ function Chart(props) {
         props.data,
       ]}
       layout={theme==='dark' ? 
-      {title: props.title, autosize: true, paper_bgcolor: '#1c1c1e', plot_bgcolor: '#1c1c1e'}:
-      {title: props.title, autosize: true, paper_bgcolor: '#f2f2f6', plot_bgcolor: '#f2f2f6'} }
+      {title: props.title, autosize: true, paper_bgcolor: '#1c1c1e', plot_bgcolor: '#1c1c1e', font: {color: '#fff'}}:
+      {title: props.title, autosize: true, paper_bgcolor: '#f2f2f6', plot_bgcolor: '#f2f2f6', font: {color: '#000'}} }
       useResizeHandler= {true}
       style= { {width: "100%", height: "100%"} }
     />
