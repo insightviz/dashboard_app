4e050c04a642d651b8f4a6eb3d7db40710e718e9
Eze Ahunanya <ezeahunanya@outlook.com>
2022-09-04 05:04:17 +0100

    Add dark and lighht background to chart

--- src/Pages/StopSearch/Chart/Chart.jsx
+++ src/Pages/StopSearch/Chart/Chart.jsx
@@ -1,12 +1,17 @@
 import Plot from 'react-plotly.js';
+import { ThemeContext } from '../../../AppTheme';
+import { useContext } from 'react';
 
 function Chart(props) {
+  const { mode } = useContext(ThemeContext);
   return (
     <Plot
       data={[
         props.data,
       ]}
-      layout={ {title: props.title, autosize: true} }
+      layout={mode==='dark' ? 
+      {title: props.title, autosize: true, paper_bgcolor: '#1c1c1e', plot_bgcolor: '#1c1c1e'}:
+      {title: props.title, autosize: true, paper_bgcolor: '#f2f2f6', plot_bgcolor: '#f2f2f6'} }
       useResizeHandler= {true}
       style= { {width: "100%", height: "100%"} }
     />
