ef98213696e51fdd8782edcec70b0f52fe2147d5
Eze Ahunanya <ezeahunanya@outlook.com>
2022-09-04 07:03:04 +0100

    Add padding to stop search page

--- src/Pages/StopSearch/StopSearch.jsx
+++ src/Pages/StopSearch/StopSearch.jsx
@@ -1,8 +1,9 @@
 import DashboardController from './Controller/Controller'
+import './StopSearch.css'
 
 function StopSearch() {
     return (
-        <div>
+        <div className='stop-search-page'>
           <h1>UK stop and search dashboard</h1>
           <DashboardController />
         </div> 
