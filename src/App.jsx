e646cbf65cd5ac7023fa7f229b7c26b5fad3a406
Eze Ahunanya <ezeahunanya@outlook.com>
2022-09-09 11:47:21 +0100

    Change date picker package

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Layout from './Layout/Layout';
 import { lazy, Suspense, useContext } from 'react';
 import { ThemeContext } from './AppTheme';
 import { GeistProvider, CssBaseline } from '@geist-ui/core'
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 
 
 const Home = lazy(() => import('./Pages/Home/Home'));
@@ -15,24 +17,26 @@ export default function App() {
   return (
     <GeistProvider themeType={theme}>
       <CssBaseline />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Layout/>}>
-            <Route index element={
-              <Suspense fallback={<>Loading...</>}>
-                <Home />
-              </Suspense>} />
-            <Route path="/stopsearch" element={
-              <Suspense fallback={<>Loading...</>}>
-                <StopSearch />
-              </Suspense>} />
-            <Route path="/contact" element={
-              <Suspense fallback={<>Loading...</>}>
-                <Contact />
-              </Suspense>} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Layout/>}>
+              <Route index element={
+                <Suspense fallback={<>Loading...</>}>
+                  <Home />
+                </Suspense>} />
+              <Route path="/stopsearch" element={
+                <Suspense fallback={<>Loading...</>}>
+                  <StopSearch />
+                </Suspense>} />
+              <Route path="/contact" element={
+                <Suspense fallback={<>Loading...</>}>
+                  <Contact />
+                </Suspense>} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </LocalizationProvider>
     </GeistProvider>
   );
   }
