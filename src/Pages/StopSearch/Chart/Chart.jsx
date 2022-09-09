3b5f52587034f4ec2a4570f1466671e463cbb0dd
Eze Ahunanya <ezeahunanya@outlook.com>
2022-09-09 08:14:15 +0100

    Fix colour on charts for light theme

--- src/Pages/StopSearch/Chart/Chart.jsx
+++ src/Pages/StopSearch/Chart/Chart.jsx
@@ -15,7 +15,7 @@ function Chart({data, ylabel, xlabel, chartWidth}) {
       {margin: {r: 0, b: 40, l: 40, t:15}, autosize: true, paper_bgcolor: '#1c1c1e', plot_bgcolor: '#1c1c1e', font: {color: '#fff'}, yaxis: {showgrid: false, title: {text: ylabel}, automargin: true, showticklabels: false}, xaxis: {title: {text: xlabel}, automargin: true, showticklabels: false}, bargap :0.1}:
       {margin: {r: 0, b: 40, l: 40, t:15}, autosize: true, paper_bgcolor: '#1c1c1e', plot_bgcolor: '#1c1c1e', font: {color: '#fff'}, yaxis: {showgrid: false, title: {text: ylabel}, automargin: true, showticklabels: false}, xaxis: {title: {text: xlabel}, automargin: true}, bargap :0.1}:
       chartWidth < 550 ?
-      {margin: {r: 0, b: 40, l: 40, t:15}, autosize: true, paper_bgcolor: '#1c1c1e', plot_bgcolor: '#1c1c1e', font: {color: '#fff'}, yaxis: {showgrid: false, title: {text: ylabel}, automargin: true, showticklabels: false}, xaxis: {title: {text: xlabel}, automargin: true, showticklabels: false}, bargap :0.1}:
+      {margin: {r: 0, b: 40, l: 40, t:15}, autosize: true, paper_bgcolor: '#f2f2f6', plot_bgcolor: '#f2f2f6', font: {color: '#000'}, yaxis: {showgrid: false, title: {text: ylabel}, automargin: true, showticklabels: false}, xaxis: {title: {text: xlabel}, automargin: true, showticklabels: false}, bargap :0.1}:
       {margin: {r: 0, b: 40, l: 40, t:15}, autosize: true, paper_bgcolor: '#f2f2f6', plot_bgcolor: '#f2f2f6', font: {color: '#000'}, yaxis: {showgrid: false, title: {text: ylabel}, automargin: true, showticklabels: false}, xaxis: {title: {text: xlabel}, automargin: true}, bargap :0.1} }
       useResizeHandler= {true}
       style= { {width: "100%", height: "100%"} }
