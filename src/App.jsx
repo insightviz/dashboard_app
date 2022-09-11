789cb15500c2dc544d6abe7790f2f807b0b1756e
Eze Ahunanya <ezeahunanya@outlook.com>
2022-09-11 07:01:06 +0100

    Add mui theme to app

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { ThemeContext } from './AppTheme';
 import { GeistProvider, CssBaseline } from '@geist-ui/core'
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 
 const Home = lazy(() => import('./Pages/Home/Home'));
@@ -14,9 +15,45 @@ const StopSearch = lazy(() => import('./Pages/StopSearch/StopSearch'));
 export default function App() {
   const { theme } = useContext(ThemeContext);
 
+  const getDesignTokens = (mode: PaletteMode) => ({
+    palette: {
+      mode,
+      ...(mode === 'light'
+        ? {
+            // palette values for light mode
+            divider: 'f9a31a',
+            text: {
+              primary:'#000',
+            },
+          }
+        : {
+            // palette values for dark mode
+            divider: 'f9a31a',
+            background: {
+              paper: '#1c1c1e'
+            },
+            text: {
+              primary: '#fff',
+            },
+          }),
+    },
+    shape: {borderRadius: 5},
+    typography: {
+      fontSize: 16,
+      fontFamily: "Sergoe"
+    }
+  });
+  
+
+  const muiTheme = createTheme(
+    getDesignTokens(theme)
+  );
+
   return (
     <GeistProvider themeType={theme}>
       <CssBaseline />
+      <ThemeProvider theme={muiTheme}>
+
       <LocalizationProvider dateAdapter={AdapterDateFns}>
         <BrowserRouter>
           <Routes>
@@ -37,6 +74,7 @@ export default function App() {
           </Routes>
         </BrowserRouter>
       </LocalizationProvider>
+      </ThemeProvider>
     </GeistProvider>
   );
   }
