91a1bb0e76708f0009518681da09450de7497be5
Eze Ahunanya <ezeahunanya@outlook.com>
2022-09-19 15:30:39 +0100

    Update config

--- config.py
+++ config.py
@@ -1,8 +1,6 @@
 import os
 
 class Config(object):
-    DEBUG = False
-    TESTING = False
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
@@ -11,7 +9,3 @@ class ProductionConfig(Config):
 
 class DevelopmentConfig(Config):
     SECRET_KEY="dev"
-    DEBUG = True
-
-class TestingConfig(Config):
-    TESTING = True
