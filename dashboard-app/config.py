7937e0470b565f7540cbaf66c984a606ba51a760
Eze Ahunanya <ezeahunanya@outlook.com>
2022-09-21 16:05:14 +0100

    Add env variables to config

--- dashboard-app/config.py
+++ dashboard-app/config.py
@@ -1,7 +1,16 @@
 import os
 
+DB_CONFIG = {
+    'database': os.environ.get('POSTGRES_DB'),
+    'user': os.environ.get('POSTGRES_USER'),
+    'password': os.environ.get('POSTGRES_PASSWORD'),
+    'host': os.environ.get('POSTGRES_HOST'),
+}
+
+CONNECTION_STRING = f"postgresql://{DB_CONFIG['user']}:{DB_CONFIG['password']}@{DB_CONFIG['host']}/{DB_CONFIG['database']}"
+
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = CONNECTION_STRING
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class ProductionConfig(Config):
