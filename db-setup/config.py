57f665c29d75f0f089e822d33bf25570acd979e4
Eze Ahunanya <ezeahunanya@outlook.com>
2022-10-14 21:08:55 +0100

    Edit config for aws deployment

--- db-setup/config.py
+++ db-setup/config.py
@@ -1,18 +1,12 @@
 import os
 from sqlalchemy.orm import declarative_base
 from sqlalchemy import create_engine
-from dotenv import load_dotenv
+import json
 
-load_dotenv()
+# gets database credentials from environment variable in aws cloud instance
+CREDENTIALS = json.loads(os.environ.get('insight_db_credentials'))
 
-DB_CONFIG = {
-    'database': os.environ.get('POSTGRES_DB'),
-    'user': os.environ.get('POSTGRES_USER'),
-    'password': os.environ.get('POSTGRES_PASSWORD'),
-    'host': os.environ.get('POSTGRES_HOST'),
-}
-
-CONNECTION_STRING = f"postgresql://{DB_CONFIG['user']}:{DB_CONFIG['password']}@{DB_CONFIG['host']}/{DB_CONFIG['database']}"
+CONNECTION_STRING = f"postgresql://{CREDENTIALS['username']}:{CREDENTIALS['password']}@{CREDENTIALS['host']}/{CREDENTIALS['database']}"
 # connect with data
 engine = create_engine(url=CONNECTION_STRING, echo=True)
 # manage tables
