694c88b8b0ca02dc455db867cc972b83b7eba19e
Eze Ahunanya <ezeahunanya@outlook.com>
2022-11-06 16:23:55 +0000

    Update config to match names used in aws db secret

--- db-setup/config.py
+++ db-setup/config.py
@@ -4,9 +4,10 @@ from sqlalchemy import create_engine
 import json
 
 # gets database credentials from environment variable in aws cloud instance
-CREDENTIALS = json.loads(os.environ.get('insight_db_credentials'))
+CREDENTIALS = json.loads(os.environ.get('insightviz_database_secret'))
+HOST = os.environ.get('proxy_endpoint')
 
-CONNECTION_STRING = f"postgresql://{CREDENTIALS['username']}:{CREDENTIALS['password']}@{CREDENTIALS['host']}/{CREDENTIALS['database']}"
+CONNECTION_STRING = f"postgresql://{CREDENTIALS['username']}:{CREDENTIALS['password']}@{HOST}/{CREDENTIALS['dbname']}"
 # connect with data
 engine = create_engine(url=CONNECTION_STRING, echo=True)
 # manage tables
