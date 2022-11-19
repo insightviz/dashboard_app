928ddf05e9d012498a324b66f569e40f9346b65a
Eze Ahunanya <ezeahunanya@outlook.com>
2022-11-19 20:58:37 +0000

    Add default theme to app in layout component

--- nextjs_frontend/components/layout/Layout.js
+++ nextjs_frontend/components/layout/Layout.js
@@ -3,6 +3,7 @@ import Footer from "../footer/Footer";
 import { useAppThemeContext } from '../../context/AppTheme';
 import Head from 'next/head';
 import { MantineProvider } from '@mantine/core';
+import { NotificationsProvider } from '@mantine/notifications';
 
 export default function Layout({ children }) {
   const { theme } = useAppThemeContext();
@@ -17,16 +18,47 @@ export default function Layout({ children }) {
         withNormalizeCSS
         theme={{
           /** Put your mantine theme override here */
-          colorScheme: {theme},
+          colorScheme: theme,
+          fontFamily: 'segoe',
+          components: {
+            Select: {
+              defaultProps:{
+                size: 'md',
+                radius: 'xl',
+                maxDropdownHeight: 400,
+                iconWidth: 60,
+              }
+            },
+            TextInput: {
+              defaultProps:{
+                iconWidth: 40,
+                radius: 'xl',
+                size: 'md',
+              }
+            },
+            Button: {
+              defaultProps:{
+                radius: 'xl',
+                size: 'md',
+              }
+            },
+            Avatar: {
+              defaultProps:{
+                radius: 'xl'
+              }
+            }
+          }
         }}
       >
-        <div className="app-wrapper">
-          <NavbarController/>
-          <main className="page-main">
-            { children }
-          </main>
-          <Footer/>
-        </div>
+        <NotificationsProvider>
+          <div className="app-wrapper">
+            <NavbarController/>
+            <main className="page-main">
+              { children }
+            </main>
+            <Footer/>
+          </div>
+        </NotificationsProvider>
       </MantineProvider>
     </>
   );
