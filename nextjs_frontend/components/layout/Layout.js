51f494539dd75500aff1826275a29a6cd224d3c8
Eze Ahunanya <ezeahunanya@outlook.com>
2022-11-21 11:26:02 +0000

    Add mui theme to layout

--- nextjs_frontend/components/layout/Layout.js
+++ nextjs_frontend/components/layout/Layout.js
@@ -4,10 +4,46 @@ import { useAppThemeContext } from '../../context/AppTheme';
 import Head from 'next/head';
 import { MantineProvider } from '@mantine/core';
 import { NotificationsProvider } from '@mantine/notifications';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 export default function Layout({ children }) {
   const { theme } = useAppThemeContext();
 
+  const getDesignTokens = (theme) => ({
+    palette: {
+      mode: theme,
+      ...(theme === 'light'
+        ? {
+            // palette values for light mode
+            divider: 'f9a31a',
+            text: {
+              primary:'#000',
+            },
+          }
+        : {
+            // palette values for dark mode
+            divider: 'ed4da',
+            background: {
+              paper: '#25262b'
+            },
+            text: {
+              primary: '#C1C2C5',
+            },
+          }),
+    },
+    shape: {borderRadius: '32px'},
+    typography: {
+      fontSize: 22.4,
+      fontFamily: 'segoe'
+    }
+  });
+
+  const muiTheme = createTheme(
+    getDesignTokens(theme)
+  );
+  
   return (
     <>
       <Head>
@@ -51,13 +87,17 @@ export default function Layout({ children }) {
         }}
       >
         <NotificationsProvider>
-          <div className="app-wrapper">
-            <NavbarController/>
-            <main className="page-main">
-              { children }
-            </main>
-            <Footer/>
-          </div>
+          <ThemeProvider theme={muiTheme}>
+            <LocalizationProvider dateAdapter={AdapterDateFns}>
+             <div className="app-wrapper">
+               <NavbarController/>
+               <main className="page-main">
+                 { children }
+               </main>
+               <Footer/>
+             </div>
+            </LocalizationProvider>
+          </ThemeProvider>
         </NotificationsProvider>
       </MantineProvider>
     </>
