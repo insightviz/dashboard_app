e34fb27206fd6161329e3c107c78448615cb8551
Eze Ahunanya <ezeahunanya@outlook.com>
2023-01-16 11:06:07 +0000

    Add paper background to line chart

--- nextjs_frontend/components/chart/LineChart.tsx
+++ nextjs_frontend/components/chart/LineChart.tsx
@@ -12,7 +12,7 @@ import {
   Colors 
 } from 'chart.js';
 import { Line } from 'react-chartjs-2';
-import { Title } from '@mantine/core';
+import { Title, Paper } from '@mantine/core';
 import { useElementSize  } from '@mantine/hooks';
 
 
@@ -39,60 +39,62 @@ function LineChart({ chartData, title, months}: lineChartProps) {
   const { ref, width } = useElementSize();
   return (
     <div className="chart-container" ref={ref}>
-      <Title order={3} align="center">{title}</Title>
-      <Line
-        data={chartData}
-        options={{
-          responsive: true,
-          plugins: {
-            title: {
-              display: false
-            },
-            legend: {
-              display: true
-            },
-            tooltip: {
-              enabled: true
-            }
-          },
-          scales: {
-            y: {
-              beginAtZero: true,
-              grid: {
-                display: false,
-              }
-            },
-            x: {
-              grid: {
-                display: false,
+      <Paper withBorder p="md" radius="md">
+        <Title order={3} align="center">{title}</Title>
+        <Line
+          data={chartData}
+          options={{
+            responsive: true,
+            plugins: {
+              title: {
+                display: false
               },
-              ticks: {
-                // For a category axis, the val is the index so the lookup via getLabelForValue is needed
-                callback: function(val, index) {
-                  if (width>=600) {
-                    if (months <= 12) {
-                      return this.getLabelForValue(Number(val))
-                    } else if (months <= 24) {
-                      // Hide every 2nd tick label
-                      return index % 2 === 0 ? this.getLabelForValue(Number(val)) : null;
-                    } else {
-                      return index % 3 === 0 ? this.getLabelForValue(Number(val)) : null;
-                    }
-                  } else {
-                    if (months <= 12) {
-                      return index % 3 === 0 ? this.getLabelForValue(Number(val)) : null;
-                    } else if (months <= 24) {
-                      // Hide every 2nd tick label
-                      return index % 6 === 0 ? this.getLabelForValue(Number(val)) : null;
-                    } else {
-                      return index % 8 === 0 ? this.getLabelForValue(Number(val)) : null;
-                    }
-                  }
+              legend: {
+                display: true
+              },
+              tooltip: {
+                enabled: true
               }
-            }}
-          }
-        }}
-      />
+            },
+            scales: {
+              y: {
+                beginAtZero: true,
+                grid: {
+                  display: false,
+                }
+              },
+              x: {
+                grid: {
+                  display: false,
+                },
+                ticks: {
+                  // For a category axis, the val is the index so the lookup via getLabelForValue is needed
+                  callback: function(val, index) {
+                    if (width>=600) {
+                      if (months <= 12) {
+                        return this.getLabelForValue(Number(val))
+                      } else if (months <= 24) {
+                        // Hide every 2nd tick label
+                        return index % 2 === 0 ? this.getLabelForValue(Number(val)) : null;
+                      } else {
+                        return index % 3 === 0 ? this.getLabelForValue(Number(val)) : null;
+                      }
+                    } else {
+                      if (months <= 12) {
+                        return index % 3 === 0 ? this.getLabelForValue(Number(val)) : null;
+                      } else if (months <= 24) {
+                        // Hide every 2nd tick label
+                        return index % 6 === 0 ? this.getLabelForValue(Number(val)) : null;
+                      } else {
+                        return index % 8 === 0 ? this.getLabelForValue(Number(val)) : null;
+                      }
+                    }
+                }
+              }}
+            }
+          }}
+        />
+      </Paper>
     </div>
   );
 }
