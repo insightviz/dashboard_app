8cc4e743d8fe843032ee246ef6f6003fb910715a
Eze Ahunanya <ezeahunanya@outlook.com>
2023-01-19 06:21:38 +0000

    Add title tag to all pages

--- nextjs_frontend/pages/stopsearch.tsx
+++ nextjs_frontend/pages/stopsearch.tsx
@@ -3,13 +3,22 @@ import { getCookie } from 'cookies-next';
 import { InferGetServerSidePropsType } from 'next'
 import React from 'react';
 import { AppContext, AppProps } from 'next/app';
+import Head from 'next/head';
 
 interface StopSearchPageProps { // <--- your custom page props
     savedForce: string,
 }
 export default function StopSearch({savedForce}: StopSearchPageProps) {
     return (
-        <StopSearchDashboardController savedForce={savedForce}/> 
+        <>
+          <Head>
+            <meta charSet="utf-8" />
+            <title>Stop and Search Dashboard</title>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <meta name='description' content='Stop and search dashboard showing data for UK police forces'/>
+          </Head>
+          <StopSearchDashboardController savedForce={savedForce}/> 
+        </>
     );
 }
 
