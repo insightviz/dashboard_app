4cb85b5885af58fbaca11e83b2767e7f63b54e4d
Eze Ahunanya <ezeahunanya@outlook.com>
2023-01-22 22:58:15 +0000

    Add get serverSideProps to each page instead of getInitialProps around _app for automatic SSG by next

--- nextjs_frontend/pages/stopsearch.tsx
+++ nextjs_frontend/pages/stopsearch.tsx
@@ -2,27 +2,35 @@ import StopSearchDashboardController from '../components/stopSearchDashboard/Sto
 import { getCookie } from 'cookies-next';
 import React from 'react';
 import Head from 'next/head';
+import Layout from '../components/layout/Layout'
+import ThemeProvider from '../context/AppTheme';
 
 interface StopSearchPageProps { // <--- your custom page props
     savedForce: string,
+    savedMode: string,
+    savedTheme: string,
 }
-export default function StopSearch({savedForce}: StopSearchPageProps) {
-    return (
-        <>
+export default function StopSearch({savedForce, savedMode, savedTheme}: StopSearchPageProps) {
+    return (        
+      <ThemeProvider savedMode={savedMode} savedTheme={savedTheme}>
+        <Layout>
           <Head>
             <meta charSet="utf-8" />
             <title>Stop and Search Dashboard</title>
             <meta name='description' content='Stop and search dashboard showing data for UK police forces'/>
           </Head>
           <StopSearchDashboardController savedForce={savedForce}/> 
-        </>
+        </Layout>
+      </ThemeProvider>        
     );
 }
 
-export async function getServerSideProps(context: any){
+export async function getServerSideProps(ctx: any){
     return {
       props: {
-          savedForce: String(getCookie('insightStopSearchForce', context) || 'metropolitan'),
+        savedForce: getCookie('insightStopSearchForce', ctx) || 'metropolitan',
+        savedMode: getCookie('insightMode', ctx) || 'system',
+        savedTheme: getCookie('insightTheme', ctx) || 'light',
       },
     }
   }
