f62dbf5df1fec16e9ec87235d7ad7bd0e78fc1b0
Eze Ahunanya <ezeahunanya@outlook.com>
2023-03-10 12:52:25 +0000

    Change sign up input

--- nextjs_frontend/components/signUpForm/SignUpForm.tsx
+++ nextjs_frontend/components/signUpForm/SignUpForm.tsx
@@ -1,9 +1,9 @@
 import styles from './SignUpForm.module.css'
-import User from '@geist-ui/icons/user'
 import AtSign from '@geist-ui/icons/atSign'
 import UserCheck from '@geist-ui/icons/userCheck'
 import { TextInput, Button } from '@mantine/core';
 import { UseFormReturnType } from '@mantine/form';
+import { useElementSize } from '@mantine/hooks';
 
 interface FormValues {
   firstName: string;
@@ -18,39 +18,34 @@ interface SignUpFormProps {
 }
 
 export default function SignUpForm({onSubmit, form, isSubmittingFormData}: SignUpFormProps) {
+  const { ref, width } = useElementSize();
+  console.log(width)
   return (
     <section className={styles.signupForm}>
       <form onSubmit={form.onSubmit(onSubmit)}>
-        <div className={styles.formTitle}>
-          <h2>Subscribe for updates on new dashboards!</h2>
-        </div>
-        <div className={styles.inputs}>
-          <TextInput
-            className={styles.firstNameContainer}
-            icon={<User size={20}/>}
-            aria-label='First name input'
-            label="First Name"
-            placeholder="First Name"
-            {...form.getInputProps('firstName')}
-          />
-          <TextInput
-            className={styles.emailContainer}
-            icon={<AtSign size={20}/>}
-            aria-label='Email input'
-            withAsterisk
-            required
-            label="Email"
-            placeholder="your@email.com"
-            {...form.getInputProps('email')}
-          />
-        </div>
-        <Button 
-          rightIcon={<UserCheck size={20} />} 
-          type="submit" 
-          variant="default" 
-          loading={isSubmittingFormData}>
-            Subscribe
-        </Button>
+        <TextInput
+          className={styles.emailContainer}
+          icon={<AtSign size={20}/>}
+          radius="xl"
+          size="lg"
+          weight={400}
+          label='Dashboard updates'
+          rightSection={
+            <Button 
+              ref={ref}
+              rightIcon={<UserCheck size={16} />} 
+              type="submit" 
+              variant="default" 
+              size='md'
+              loading={isSubmittingFormData}>
+                Subscribe
+            </Button>
+          }
+          placeholder="Enter email"
+          aria-label='Email input'
+          {...form.getInputProps('email')}
+          rightSectionWidth={width+45.2}
+        />
       </form>
     </section>
   );
